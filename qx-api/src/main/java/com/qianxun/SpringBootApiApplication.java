ce66beaa0a44540011cb798f6f8f74baf5113551
zhangyp <zhangyp@singularityclub.net>
2018-05-09 22:56:07 +0800

    swagger信息配置

--- qx-api/src/main/java/com/qianxun/SpringBootApiApplication.java
+++ qx-api/src/main/java/com/qianxun/SpringBootApiApplication.java
@@ -3,7 +3,16 @@ package com.qianxun;
 import com.qianxun.config.FlyWayConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Import(FlyWayConfig.class)
@@ -11,7 +20,42 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class SpringBootApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiApplication.class, args);
+    }
+
+    /**
+     * swagger
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .genericModelSubstitutes(DeferredResult.class)
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(false)
+                .pathMapping("/")
+                .select()
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("千寻api")
+                .description("千寻API工具")
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .build();
+    }
+    /**
+     * 跨域
+     * @return
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
