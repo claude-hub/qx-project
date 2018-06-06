6ab3a0221cc10bea046d0eb26b93640968af8099
zhangyp <zhangyp@singularityclub.net>
2018-06-07 01:00:50 +0800

    gradle版本初始化

---
+++ src/main/java/com/qianxun/QxProjectApplication.java
@@ -0,0 +1,12 @@
+package com.qianxun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QxProjectApplication.class, args);
+	}
+}
