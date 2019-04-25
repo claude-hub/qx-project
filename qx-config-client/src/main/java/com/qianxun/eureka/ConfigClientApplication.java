1514b8fc4f0fa26541df6ccf5681b5824ac1ab17
Carl <314705487@qq.com>
2019-04-26 00:22:48 +0800

    config-client + github

---
+++ qx-config-client/src/main/java/com/qianxun/eureka/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.qianxun.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Cloudy
+ * Date: 2019/4/25 23:47
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
