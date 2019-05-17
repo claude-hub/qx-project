e41008df7205bc97461f5f081d42f0a7605f7724
Carl <314705487@qq.com>
2019-05-18 00:00:35 +0800

    网关

--- qx-auth/src/main/java/com/qianxun/auth/AuthApplication.java
+++ qx-auth/src/main/java/com/qianxun/auth/AuthApplication.java
@@ -4,6 +4,7 @@ import com.qianxun.auth.component.FilterIgnoreProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Cloudy
@@ -12,6 +13,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  * 报错Not registered via @EnableConfigurationProperties or marked as Spring component
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigurationProperties({FilterIgnoreProperties.class})
 public class AuthApplication {
     public static void main(String[] args) {
---
+++ qx-gateway/src/main/java/com/qianxun/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.qianxun.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 网关
+ *
+ * @author Cloudy
+ * Date: 2019/5/17 22:32
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
