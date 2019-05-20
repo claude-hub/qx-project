07af662823f19531e15ac76d5bde5eaf497ba189
Carl <314705487@qq.com>
2019-05-20 20:52:06 +0800

    rename

--- qx-auth-sso/src/main/java/com/qianxun/sso/AuthSSOApplication.java
+++ qx-auth-sso/src/main/java/com/qianxun/sso/AuthSSOApplication.java
@@ -2,15 +2,14 @@ package com.qianxun.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
-public class AuthorizationServerApplication extends SpringBootServletInitializer {
+public class AuthSSOApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationServerApplication.class, args);
+        SpringApplication.run(AuthSSOApplication.class, args);
     }
 
 }
