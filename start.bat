1157639e0a34c2f42a5e21b18ce1ca4f7f6a0a14
Carl <314705487@qq.com>
2019-05-20 11:36:16 +0800

    update

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
-java -jar qx-eureka/target/qx-eureka.jar
+start java -jar qx-eureka/target/qx-eureka.jar
 
-java -jar qx-gateway/target/qx-gateway.jar
+start java -jar qx-gateway/target/qx-gateway.jar
 
-java -jar qx-generator/target/qx-generator.jar
+start java -jar qx-generator/target/qx-generator.jar
 
-java -jar qx-user/qx-user-provider/target/qx-user-provider.jar
+start java -jar qx-user/qx-user-provider/target/qx-user-provider.jar
 
-java -jar qx-user/qx-user-consumer/target/qx-user-consumer.jar
+start java -jar qx-user/qx-user-consumer/target/qx-user-consumer.jar
