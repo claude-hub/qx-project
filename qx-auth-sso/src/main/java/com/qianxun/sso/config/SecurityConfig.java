3a28baf46010e7ba03b36999d72b0a5e8d1c5ca5
Carl <314705487@qq.com>
2019-05-24 00:10:14 +0800

    带重新

--- qx-auth-sso/src/main/java/com/qianxun/sso/config/SecurityConfig.java
+++ qx-auth-sso/src/main/java/com/qianxun/sso/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.qianxun.sso.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -10,9 +11,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @Order(1)
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,6 +27,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .permitAll()
                 .and().csrf().disable();
+
+//        http
+//                .formLogin().loginPage("/authentication/require")
+//                .loginProcessingUrl("/authentication/form")
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers(
+//                        "/login",
+//                        "/authentication/require",
+//                        "/authentication/form",
+//                        "/oauth/authorize",
+//                        "/js/common.js",
+//                        "/js/jquery.min.js",
+//                        "/css/common.css",
+//                        "/css/font-awesome.min.css",
+//                        "/css/reset.css",
+//                        "/css/style.css",
+//                        "/images/banner.png",
+//                        "/images/cut.jpg",
+//                        "/images/logo_bg.jpg",
+//                        "/images/logowz.png",
+//                        "/fonts/fontawesome-webfont.woff2"
+//                )
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .csrf().disable();
     }
 
     @Override
