443463d468353cf7ddead94f713f1ab93453bada
Carl <314705487@qq.com>
2019-05-24 16:03:45 +0800

    自定义登录界面

--- qx-auth-sso/src/main/java/com/qianxun/sso/config/SecurityConfig.java
+++ qx-auth-sso/src/main/java/com/qianxun/sso/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -16,6 +17,31 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @EnableGlobalMethodSecurity(prePostEnabled = true) //支持权限注解 @PreAuthorize("hasAnyRole('ROLE_USER')")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
+
+    /**
+     * 让Security 忽略这些url，不做拦截处理
+     *
+     * @param
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers
+                (
+                        "/js/common.js",
+                        "/js/jquery.min.js",
+                        "/css/common.css",
+                        "/css/font-awesome.min.css",
+                        "/css/reset.css",
+                        "/css/style.css",
+                        "/images/banner.png",
+                        "/images/cut.jpg",
+                        "/images/logo_bg.jpg",
+                        "/images/logowz.png",
+                        "/fonts/fontawesome-webfont.woff2"
+                );
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,49 +56,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .permitAll()
 //                .and().csrf().disable();
 
-        http.requestMatchers()
-                .antMatchers("/login",
-                        "/oauth/authorize",
-                        "/authentication/form",
-                        "/tiger-login.html")
-                .and()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/tiger-login.html")
-                .loginProcessingUrl("/authentication/form")
-                .permitAll()
-                .and().csrf().disable();
-
-//        http
-//                .formLogin().loginPage("/authentication/require")
-//                .loginProcessingUrl("/authentication/form")
+//        http.requestMatchers()
+//                .antMatchers("/login",
+//                        "/oauth/authorize",
+//                        "/authentication/form",
+//                        "/tiger-login.html")
 //                .and()
 //                .authorizeRequests()
-//                .antMatchers(
-//                        "/login",
-//                        "/authentication/require",
-//                        "/authentication/form",
-//                        "/oauth/authorize",
-//                        "/js/common.js",
-//                        "/js/jquery.min.js",
-//                        "/css/common.css",
-//                        "/css/font-awesome.min.css",
-//                        "/css/reset.css",
-//                        "/css/style.css",
-//                        "/images/banner.png",
-//                        "/images/cut.jpg",
-//                        "/images/logo_bg.jpg",
-//                        "/images/logowz.png",
-//                        "/fonts/fontawesome-webfont.woff2"
-//                )
-//                .permitAll()
 //                .anyRequest()
 //                .authenticated()
 //                .and()
-//                .csrf().disable();
+//                .formLogin()
+//                .loginPage("/tiger-login.html")
+//                .loginProcessingUrl("/authentication/form")
+//                .permitAll()
+//                .and().csrf().disable();
+
+        http
+                .requestMatchers()
+                .antMatchers(
+                        "/authentication/require",
+                        "/login",
+                        "/authentication/form", //放开登录方法
+                        "/oauth/authorize")
+                .and()
+                .authorizeRequests()
+                // 自定义页面或处理url,如果不配置全局允许，浏览器会提示服务器将页面转发多次
+                .antMatchers(
+                        "/authentication/require"
+                )
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+        http
+                .formLogin().loginPage("/authentication/require")
+                .loginProcessingUrl("/authentication/form")
+                .and()
+                .csrf().disable();
     }
 
     @Override
