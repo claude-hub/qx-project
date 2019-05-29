3f8ef3dd33e4ba116102514f1c50589760ee4426
Carl <314705487@qq.com>
2019-05-29 14:36:52 +0800

    update

--- qx-config/src/main/java/com/qianxun/config/Password.java
+++ qx-config/src/main/java/com/qianxun/config/Password.java
@@ -1,20 +1,20 @@
-package com.qianxun.config;
+// package com.qianxun.config;
 
-import org.jasypt.util.text.BasicTextEncryptor;
+// import org.jasypt.util.text.BasicTextEncryptor;
 
-/**
- * @author Cloudy
- * Date: 2019/5/27 2:16
- */
-public class Password {
-    public static void main(String[] args) {
-        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
-        //加密所需的salt(盐)
-        textEncryptor.setPassword("cloudy");
-        //要加密的数据（数据库的用户名或密码）
-        String username = textEncryptor.encrypt("root");
-        String password = textEncryptor.encrypt("root");
-        System.out.println("username:"+username);
-        System.out.println("password:"+password);
-    }
-}
+// /**
+//  * @author Cloudy
+//  * Date: 2019/5/27 2:16
+//  */
+// public class Password {
+//     public static void main(String[] args) {
+//         BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+//         //加密所需的salt(盐)
+//         textEncryptor.setPassword("cloudy");
+//         //要加密的数据（数据库的用户名或密码）
+//         String username = textEncryptor.encrypt("root");
+//         String password = textEncryptor.encrypt("root");
+//         System.out.println("username:"+username);
+//         System.out.println("password:"+password);
+//     }
+// }
